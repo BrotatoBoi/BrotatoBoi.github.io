ef59e8f5f790b753bc7a62e91a49068acd5c357d
BrotatoBoi <33473023+BrotatoBoi@users.noreply.github.com>
2021-05-26 20:41:48 -0400

    Update script.js

--- script.js
+++ script.js
@@ -2,11 +2,7 @@ $(document).ready(function() {
   $('#download').hide();
 })
 
-function setup() {
-  document.getElementById('download').style.visibility = "hidden";
-}
-
 function switchTab(fromId, toId) {
-  document.getElementById(fromId).style.visibility = "visible";
-  document.getElementById(toId).style.visibility = "hidden";
+  $(fromId).show();
+  $(toId).hide();
 }
