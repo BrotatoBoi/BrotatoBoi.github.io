b3195ab7a6aeb39f95263be92421e32d0a8a8e11
Bruhtato <flamingburrito15@gmail.com>
2021-07-08 19:29:27 +0000

    Added Game div

--- script.js
+++ script.js
@@ -2,7 +2,10 @@ $(document).ready(function() {
   $('#download').hide();
 })
 
-function switchTab(fromId, toId) {
-  $(fromId).show();
-  $(toId).hide();
+function switchTab(goToTab) {
+  var navDivs = document.getElementsByClassName("navDivs")
+
+  for(let i=0; i<navDivs.length; i++) {
+    console.log(navDivs)
+  }
 }
