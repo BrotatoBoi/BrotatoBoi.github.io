dee18815d5eedaf893151321238abc1844afd351
Bruhtato <flamingburrito15@gmail.com>
2021-07-19 08:53:51 -0700

    Fixed the divs?

--- script.js
+++ script.js
@@ -3,6 +3,11 @@ $(document).ready(function() {
 })
 
 function switchTab(goToTab) {
-  document.getElementsByClassName("navDivs").style.visibility = 'hidden';
+  navDivs = document.getElementsByClassName("navDivs");
+  
+  for(let i=0; i<navDivs.length; i++) {
+    navDivs[i].style.visibility = 'hidden';
+  }
+  
   document.getElementsById(goToTab).style.visibility = 'visible';
 }
