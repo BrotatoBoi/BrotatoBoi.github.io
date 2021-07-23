80aaeb787740460060452cde7142430e70c38ac3
Bruhtato <flamingburrito15@gmail.com>
2021-07-22 19:29:05 -0700

    Added the script

---
+++ Portfolio/script.js
@@ -0,0 +1,9 @@
+function switchTab(tabID) {
+    var navDivs = document.getElementsByClassName("navDiv");
+
+    for(let i=0; i<navDivs.length; i++) {
+        navDivs[i].style.display = "none";
+    }
+
+    document.getElementById(tabID).style.display = "block";
+}
