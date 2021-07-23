b4846be6a61f4e471e8e1dd6de49930bb326cbdf
Bruhtato <flamingburrito15@gmail.com>
2021-07-23 07:57:30 -0700

    fixed a dumb typo

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 function switchTab(tabID) {
-  document.getElementsBtClassName('.navDiv').style.display = "none";
+  document.getElementsByClassName('.navDiv').style.display = "none";
   document.getElementById(tabID).style.display = "block";
 }
