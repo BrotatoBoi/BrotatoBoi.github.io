3a2c2a7828404f5a8cd0b315f498e724014f3798
Bruhtato <flamingburrito15@gmail.com>
2021-07-23 08:30:58 -0700

    fixed variable line

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function switchTab(tabID) {
-  var navDivs = document.getElementsByClassName('navDiv').style.display = "none";
+  var navDivs = document.getElementsByClassName('navDiv');
   
   for(let i=0; i<navDivs.length; i++) {
     navDivs[i].style.display = "none";
