45427a6b81fca36b50a5e21b6e6b0b4319a38b78
Bruhtato <flamingburrito15@gmail.com>
2021-07-28 08:04:12 -0700

    .

---
+++ PI/script.js
@@ -0,0 +1,13 @@
+// Permissions to get from user.
+const scopes = ['payments'];
+
+// There is something in the SDK about this, plz read it!
+function onIncompletePaymentFound(payment) {
+    console.log('There must have been an incomplete payment!');
+}
+
+Pi.authenticate(scopes, onIncompletePaymentFound).then(function(auth) {
+    console.log('Hey! You may now purchase something!');
+}).catch(function(error){
+    console.error(error);
+});
