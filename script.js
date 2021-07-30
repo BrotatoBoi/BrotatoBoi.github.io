2334ca59b83110e20c23b5e82e5c13c1c4e1484a
Bruhtato <flamingburrito15@gmail.com>
2021-07-30 14:37:09 -0700

    Deleted script

--- script.js
+++
@@ -1,9 +0,0 @@
-function switchTab(tabID) {
-  var navDivs = document.getElementsByClassName('navDiv');
-  
-  for(let i=0; i<navDivs.length; i++) {
-    navDivs[i].style.display = "none";
-  }
-  
-  document.getElementById(tabID).style.display = "block";
-}
